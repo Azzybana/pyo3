8b4f2efaa3dc5083bb8cf7738523ed972ec73d96
Azzybana Raccoon <121582001+Azzybana@users.noreply.github.com>
2025-11-13 23:41:36 -0500

    ♻️ style: refine object usage for clarity in tupleobject.rs

--- pyo3-ffi/src/cpython/tupleobject.rs
+++ pyo3-ffi/src/cpython/tupleobject.rs
@@ -1,4 +1,4 @@
-use crate::object::*;
+use crate::object::{PyVarObject, PyObject, Py_SIZE};
 #[cfg(Py_3_14)]
 use crate::pyport::Py_hash_t;
 #[cfg(not(PyPy))]
@@ -27,14 +27,14 @@ pub unsafe fn PyTuple_GET_SIZE(op: *mut PyObject) -> Py_ssize_t {
 #[inline]
 #[cfg(not(any(PyPy, GraalPy)))]
 pub unsafe fn PyTuple_GET_ITEM(op: *mut PyObject, i: Py_ssize_t) -> *mut PyObject {
-    *(*(op as *mut PyTupleObject)).ob_item.as_ptr().offset(i)
+    *(*op.cast::<PyTupleObject>()).ob_item.as_ptr().offset(i)
 }
 
 /// Macro, *only* to be used to fill in brand new tuples
 #[inline]
 #[cfg(not(any(PyPy, GraalPy)))]
 pub unsafe fn PyTuple_SET_ITEM(op: *mut PyObject, i: Py_ssize_t, v: *mut PyObject) {
-    *(*(op as *mut PyTupleObject)).ob_item.as_mut_ptr().offset(i) = v;
+    *(*op.cast::<PyTupleObject>()).ob_item.as_mut_ptr().offset(i) = v;
 }
 
 // skipped _PyTuple_DebugMallocStats
