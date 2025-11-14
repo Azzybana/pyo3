2059adc7953f7ada83d7fcddc0f0946a6e420e1b
Azzybana Raccoon <121582001+Azzybana@users.noreply.github.com>
2025-11-13 23:05:39 -0500

    ♻️ style: fix markdown formatting in LitStrValue documentation

--- pyo3-macros-backend/src/attributes.rs
+++ pyo3-macros-backend/src/attributes.rs
@@ -186,7 +186,7 @@ pub struct OptionalKeywordAttribute<K, V> {
 }
 
 /// A helper type which parses the inner type via a literal string
-/// e.g. `LitStrValue<Path>` -> parses "some::path" in quotes.
+/// e.g. `LitStrValue<Path>` -> parses "`some::path`" in quotes.
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct LitStrValue<T>(pub T);
 
