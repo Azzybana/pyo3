3ccc33f99d7af49c120a5c5457ce020778aa5b04
Azzybana Raccoon <121582001+Azzybana@users.noreply.github.com>
2025-11-13 22:54:26 -0500

    🔁 style: use `.into()` instead of `as usize` for bool-to-usize conversion

--- pyo3-macros-backend/src/pyfunction.rs
+++ pyo3-macros-backend/src/pyfunction.rs
@@ -370,7 +370,7 @@ pub fn impl_wrap_pyfunction(
         .sig
         .inputs
         .iter_mut()
-        .skip(tp.skip_first_rust_argument_in_python_signature() as usize)
+        .skip(tp.skip_first_rust_argument_in_python_signature().into())
         .map(FnArg::parse)
         .try_combine_syn_errors()?;
 
